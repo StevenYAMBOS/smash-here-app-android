f8beff1227727d7662d940afd9dabf04009a6d53
StevenYAMBOS <stevenyambos@gmail.com>
2023-05-31 20:23:15 +0200

    Création du model FighterSate

---
+++ app/src/main/java/com/stevenyambos/smashhereapp/screens/FighterState.kt
@@ -0,0 +1,12 @@
+package com.stevenyambos.smashhereapp.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stevenyambos.smashhereapp.R
+
+class FighterState : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fighter_state)
+    }
+}
